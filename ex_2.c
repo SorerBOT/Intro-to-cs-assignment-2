47e21dcaaae6685aa455f385b201fc727af53b37
SorerBOT <alonfiller@gmail.com>
2024-01-28 17:42:31 +0200

    (Feat) finished the assignment

--- ex_2.c
+++ ex_2.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+int scanNumberVerbose();
+int isPrime(int number);
 int playGame();
 int validateQuestNumber(int chosenQuestNumber);
 void printError(int numRequired);
@@ -14,7 +16,22 @@ int main() {
 	while (!hasUserQuit) hasUserQuit = playGame();
 	return 0;
 }
+int isPrime(int number) {
+	if (number == 2) return 1;
+	if (number % 2 == 0) return 0;
+	for (int i = 3; i <= number/2; i += 2) {
+		if (number % i == 0) return 0;
+	}
 
+	return 1;
+}
+int scanNumberVerbose() {
+	int number;
+	printf("Enter a number: ");
+	scanf("%d", &number);
+
+	return number;
+}
 int playGame() {
 	int chosenQuestNumber;
 	printf("Welcome to Thanos's playground.\n");
@@ -24,29 +41,32 @@ int playGame() {
 	printf("3. Thanos' Maze System\n");
 	printf("4. The Sorcery of Thanos\n");
 	printf("5. Quit the quest\n");
-
+	printf("\n");
 	scanf("%d", &chosenQuestNumber);
 
 	if (!validateQuestNumber(chosenQuestNumber)) return 0;
 	if (chosenQuestNumber == 1) return firstQuest();
+	if (chosenQuestNumber == 2) return secondQuest();
+	if (chosenQuestNumber == 3) return thirdQuest();
+	if (chosenQuestNumber == 4) return fourthQuest();
+	if (chosenQuestNumber == 5) return fifthQuest();
 	return 0;
 }
 
 int validateQuestNumber(int chosenQuestNumber) {
 	if (chosenQuestNumber < 1 || chosenQuestNumber > 5) {
-		printf("Thanos is mad! You are playing his game and this is not an option. Choose again, wisely\n");
+		printf("Thanos is mad! You are playing his game and this is not an option. Choose again, wisely.\n");
+		printf("\n");
 		return 0;
 	}
 	return 1;
 }
 void printError(int numRequired) {
 	printf("Foolish mortal! You cannot solve Thanos's quest with an input that isn't a number bigger than %d\n", numRequired);
+	printf("\n");
 }
 int firstQuest() {
-	int number;
-	printf("Enter a number: ");
-	scanf("%d", &number);
-	
+	int number = scanNumberVerbose();
 	if (number <= 1) {
 		printError(1);
 		return 0;
@@ -58,5 +78,64 @@ int firstQuest() {
 		if (i % 5 && i % 3) printf("%d", i);
 		printf("\n");
 	}
+	printf("\n");
 	return 0;
+}
+
+int secondQuest() {
+	int n = scanNumberVerbose();
+	if (n <= 0) {
+		printError(0);
+		return 0;
+	}
+	int current = 1, previous = 0;
+	
+	for (int i = 1; i <= n; i++) {
+		printf("%d", previous);
+		current += previous;
+		previous = current - previous;
+		if (i == n) printf("\n");
+		else printf(" ");
+	}
+	printf("\n");
+	return 0;
+}
+
+int thirdQuest() {
+	int n = scanNumberVerbose();
+	if (n <= 1) {
+		printError(1);
+		return 0;
+	}
+	
+	int sum = 0;
+	for (int i = 2; i <= n; i++) {
+		if (isPrime(i)) sum += i;
+	}
+	printf("%d\n", sum);
+	printf("\n");
+	return 0;
+}
+
+int fourthQuest() {
+	int n = scanNumberVerbose();
+	if (n <= 0) {
+		printError(0);
+		return 0;
+	}
+	int sum = 0;
+	for (int i = 1; i < n; i++) {
+		if (n % i == 0) sum += i;
+	}
+
+	if (sum == n) printf("Perfect!\n");
+	else printf("Not Perfect!\n");
+	
+	printf("\n");
+	return 0;
+}
+
+int fifthQuest() {
+	printf("Congratulations! You finished the quest and managed to defeat Thanos\n");
+	return 1;
 }
